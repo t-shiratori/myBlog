98ed9802478f4a7c5e44df602342ca93a3be7615
白鳥貴士 <tks.srtr89@gmail.com>
2021-09-29 02:23:35 +0900

    header修正

--- components/GlobalHeader/index.tsx
+++ components/GlobalHeader/index.tsx
@@ -6,10 +6,11 @@ const GlobalHeader = (): JSX.Element => {
 			<div className="p-4 w-full flex justify-center bg-gray-100 tablet:hidden">
 				<h2 className="text-lg">Blog</h2>
 			</div>
-
-			<div className="hidden p-4 justify-between bg-indigo-600 tablet:flex">
-				<div>
-					<NavLink href={'/'} name={'Blog'} />
+			<div className="hidden tablet:block w-full bg-indigo-600">
+				<div className=" p-4 justify-between tablet:flex max-w-[1024px] mx-auto">
+					<div>
+						<NavLink href={'/'} name={'Blog'} />
+					</div>
 				</div>
 			</div>
 		</header>
