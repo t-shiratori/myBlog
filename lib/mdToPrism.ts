ee2b361bf606d5196d4c1e4179add747f0c4b52d
白鳥貴士 <tks.srtr89@gmail.com>
2021-10-03 12:54:12 +0900

    iframeを表示させる

--- lib/mdToPrism.ts
+++ lib/mdToPrism.ts
@@ -1,6 +1,7 @@
 import { unified } from 'unified'
 import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
+import rehypeRaw from 'rehype-raw'
 import rehypeFormat from 'rehype-format'
 import rehypePrismPlus from 'rehype-prism-plus'
 import remarkGfm from 'remark-gfm'
@@ -10,7 +11,8 @@ const mdToPrism = async (markdown: string): Promise<string> => {
 	const result = await unified()
 		.use(remarkParse) // マークダウンのパーサー
 		.use(remarkGfm) // Autolink literals、Strikethrough、Table、Tasklist、に対応するためのremarkのプラグイン。
-		.use(remarkRehype) // マークダウンをhtmlに変換するremarkのプラグイン
+		.use(remarkRehype, { allowDangerousHtml: true }) // マークダウンをhtmlに変換するremarkのプラグイン
+		.use(rehypeRaw) // 最初からHTMLで書かれていた生のHTML部分をパースするためのプラグイン
 		.use(rehypeFormat) // htmlをきれいに整形するrehypeプラグイン
 		.use(rehypeStringify) // htmlをシリアライズするunified用のコンパイラ
 		.use(rehypePrismPlus) // PrismでHTMLのコードブロックを強調表示するrehypeプラグイン
