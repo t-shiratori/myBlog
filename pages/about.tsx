f626373399f4f2e5f56d61d1f3b74af2188e5ad0
白鳥貴士 <tks.srtr89@gmail.com>
2021-10-11 01:11:45 +0900

    Aboutを修正

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,15 +1,8 @@
-import { GetStaticProps } from 'next'
 import Head from 'next/head'
-import { TField } from '../types/article'
-import { createClient, Entry } from 'contentful'
 import Layout from '../components/Layout'
 import AboutStyles from '../styles/about.module.css'
 
-type THomeProps = {
-	articles: Entry<TField>[]
-}
-
-const Home = ({ articles }: THomeProps): JSX.Element => {
+const About = (): JSX.Element => {
 	return (
 		<Layout>
 			<>
@@ -43,27 +36,4 @@ const Home = ({ articles }: THomeProps): JSX.Element => {
 	)
 }
 
-export default Home
-
-export const getStaticProps: GetStaticProps = async () => {
-	const client = createClient({
-		space: process.env.CONTENTFUL_SPACE_ID as string,
-		accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
-	})
-
-	const getArticleParams: { [key: string]: string } = { content_type: 'article' }
-
-	if (process && process.env.NODE_ENV === 'production') {
-		// プロダクションの場合はダミーの記事を除外する
-		getArticleParams['metadata.tags.sys.id[nin]'] = 'dummyArticle'
-	}
-
-	const { items } = await client.getEntries(getArticleParams)
-
-	return {
-		props: {
-			articles: items,
-		},
-		revalidate: 1,
-	}
-}
+export default About
