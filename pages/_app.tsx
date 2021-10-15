65f7d5600ad6ffe1b289810e4cded4656322f814
白鳥貴士 <tks.srtr89@gmail.com>
2021-10-15 22:51:51 +0900

    ga導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
+import { useEffect } from 'react'
 import { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
 import '../styles/global.css'
 import '../styles/prism-night-owl.css'
 import '../styles/article/content.css'
+import * as gtag from '../lib/gtag'
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
+	// Google Analytics用の処理
+	const router = useRouter()
+	useEffect(() => {
+		const handleRouteChange = (url: string) => {
+			gtag.pageview(url)
+		}
+		router.events.on('routeChangeComplete', handleRouteChange)
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange)
+		}
+	}, [router.events])
+
 	return <Component {...pageProps} />
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID
 
 class MyDocument extends Document {
 	static async getInitialProps(ctx: DocumentContext) {
@@ -16,6 +17,25 @@ class MyDocument extends Document {
 						href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap"
 						rel="stylesheet"
 					/>
+
+					{/* Google Analytics */}
+					{GA_TRACKING_ID && (
+						<>
+							<script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+							<script
+								dangerouslySetInnerHTML={{
+									__html: `
+								window.dataLayer = window.dataLayer || [];
+								function gtag(){dataLayer.push(arguments);}
+								gtag('js', new Date());
+								gtag('config', '${GA_TRACKING_ID}', {
+									page_path: window.location.pathname,
+								});
+							`,
+								}}
+							/>
+						</>
+					)}
 				</Head>
 				<body>
 					<Main />
